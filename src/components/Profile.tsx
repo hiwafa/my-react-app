ad218ac2ef7f8037b56cfb1406289d227db17945
hiwafa <mq.func@gmail.com>
2024-06-05 16:14:24 +0200

    function type interface

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -9,11 +9,16 @@ interface ProfileTypes {
 
 function Profile(props: ProfileTypes) {
 
-    let obj: {
+    // let obj: {
+    //     readonly phone: number;
+    //     text: string
+    // }
+   
+    interface objType {
         readonly phone: number;
         text: string
     }
-
+    let obj: objType;
     obj = {
         phone: 9999,
         text: "How are you?"
@@ -22,9 +27,22 @@ function Profile(props: ProfileTypes) {
     // obj.phone = 3938484; // we can not use this, while it is readonly
     obj.text = "How is it going?"
 
+
+
+
+    interface funcTYpe {
+        (info: string, aktiv: boolean): string
+    }
+
+    let myFunc: funcTYpe = function (info, aktiv){
+        return info;
+    }
+
     return (
         <div>
             {props.message} - {props.isMen && "Sex: Men"} - {obj.text}
+            <br />
+            {myFunc("A test info", true)}
         </div>
     );
 }
